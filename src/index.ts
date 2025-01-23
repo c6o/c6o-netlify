59ff82ce6e1a0388c7fcd9cef013c940bbd5e8c0
Narayan Sainaney <narayan@sainaney.com>
2025-01-23 15:48:31 -0800

    removed all safeties

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,13 @@ import { NetlifyExtension } from "@netlify/sdk";
 const extension = new NetlifyExtension();
 
 extension.addFunctions("./src/functions", {
-  prefix: "codezero-netlify",
+  prefix: "lm0he2wq-codezero-extension",
   shouldInjectFunction: () => {
     console.log('NSX')
     // If the function is not enabled, return early
-    if (!process.env["C6O_NETLIFY_ENABLED"]) {
-      return;
-    }
+    //if (!process.env["C6O_NETLIFY_ENABLED"]) {
+    //  return;
+    //}
     return true;
   },
 });
