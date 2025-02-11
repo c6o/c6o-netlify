dd29d9f8c68cc3a68e1966359ad9202b27a7c3a7
Georg Friedrich <georg@codezero.io>
2025-02-11 11:23:35 +1100

    only inject function when extension is configured

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,9 @@ const extension = new NetlifyExtension();
 
 extension.addFunctions("./src/functions", {
   prefix: "lm0he2wq-codezero-extension",
-  shouldInjectFunction: (x, y, z) => {
-    console.log("NSX should", x, y, z)
-    // If the function is not enabled, return early
-    //if (!process.env["C6O_NETLIFY_ENABLED"]) {
-    //  return;
-    //}
-    return true;
+  shouldInjectFunction: ({ name }) => {
+    return !!process.env.CZ_SPACE_ID;
   },
-});
-
-extension.addBuildEventHandler('onPreBuild', async (x) => {
-
-  console.log("NSX", x.constants)
 });
 
 export { extension };
